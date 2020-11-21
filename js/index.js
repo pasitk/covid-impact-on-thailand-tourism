c9aa052c8195580bf4867ca42b379adbbb01cdd6
PASIT KONGKUNAKORNKUL <KongkunakornkulP@cardiff.ac.uk>
2020-11-22 05:06:22 +0700

    Intro page alomost complete

--- js/index.js
+++ js/index.js
@@ -16,6 +16,31 @@ function showItemSmoothly(isBox, item) { // isBox: yes = box, no = text
     var queryItemText = isBox ? "#index-content-item-" + (item.index + 1) : "#index-content-item-" + (item.index + 1) + "-text";
     var delayMillisec = isBox ? 0:250;
 
+    runEffectToShowItem(queryItemText,delayMillisec);
+}
+
+function showItemOnRenderSmoothly(isBox, itemIndex) { 
+    var queryItemText = isBox ? "#index-content-item-" + (itemIndex) : "#index-content-item-" + (itemIndex) + "-text";
+    var delayMillisec = isBox ? 0:250;
+
+    runEffectToShowItem(queryItemText,delayMillisec);
+}
+
+function hideItemOnRender(isBox, itemIndex, isUp) { // isBox: yes = box, no = text / isUp: yes = up, no = down
+    var transformText = isUp ? "translateY(-20%)" : "translateY(20%)";
+    var queryItemText = isBox ? "#index-content-item-" + itemIndex : "#index-content-item-" + itemIndex + "-text";
+    $(queryItemText).css({
+        'opacity': 0,
+        'transform': transformText,
+        '-webkit-transition': 'opacity 0s ease-in-out, transform 0s',
+        '-moz-transition': 'opacity 0s ease-in-out, transform 0s',
+        '-ms-transition': 'opacity 0s ease-in-out, transform 0s',
+        '-o-transition': 'opacity 0s ease-in-out, transform 0s',
+        'transition': 'opacity 0s ease-in-out, transform 0s'
+    });
+}
+
+function runEffectToShowItem(queryItemText, delayMillisec) {
     $(queryItemText)
         .delay(delayMillisec)
         .queue(function (next) {
