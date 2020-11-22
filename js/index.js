305c813895030407f009289160aeb152026bbd11
PASIT KONGKUNAKORNKUL <KongkunakornkulP@cardiff.ac.uk>
2020-11-22 17:34:32 +0700

    Add photo and video credit to intro page

--- js/index.js
+++ js/index.js
@@ -12,18 +12,54 @@ function hideItemSmoothly(isBox, item, isUp) { // isBox: yes = box, no = text / 
     });
 }
 
-function showItemSmoothly(isBox, item) { // isBox: yes = box, no = text
-    var queryItemText = isBox ? "#index-content-item-" + (item.index + 1) : "#index-content-item-" + (item.index + 1) + "-text";
-    var delayMillisec = isBox ? 0:250;
+function hideBtnSmoothly(item) {
+    var queryItemText = "#index-content-item-" + (item.index + 1) + "-btn";
+    $(queryItemText).css({
+        'opacity': 0,
+        '-webkit-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+        '-moz-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+        '-ms-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+        '-o-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+        'transition': 'opacity 0.5s ease-in-out, transform 0.5s'
+    });
 
-    runEffectToShowItem(queryItemText,delayMillisec);
+    if (item.index == 2) {
+        var queryItemText = "#index-content-item-" + (item.index + 2) + "-btn";
+        $(queryItemText).css({
+            'opacity': 0,
+            '-webkit-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+            '-moz-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+            '-ms-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+            '-o-transition': 'opacity 0.5s ease-in-out, transform 0.5s',
+            'transition': 'opacity 0.5s ease-in-out, transform 0.5s'
+        });
+    }
 }
 
-function showItemOnRenderSmoothly(isBox, itemIndex) { 
-    var queryItemText = isBox ? "#index-content-item-" + (itemIndex) : "#index-content-item-" + (itemIndex) + "-text";
-    var delayMillisec = isBox ? 0:250;
+function showItemSmoothly(isBox, item) { // isBox: yes = box, no = text
+    var queryItemText = isBox ? "#index-content-item-" + (item.index + 1) : "#index-content-item-" + (item.index + 1) + "-text";
+    var delayMillisec = isBox ? 0 : 250;
 
-    runEffectToShowItem(queryItemText,delayMillisec);
+    runEffectToShowItem(queryItemText, delayMillisec);
+}
+
+function showBtnSmoothly(item) {
+    runEffectToShowItem("#index-content-item-" + (item.index + 1) + "-btn", 250);
+
+    if (item.index == 2) {
+        runEffectToShowItem("#index-content-item-" + (item.index + 2) + "-btn", 250);
+    }
+}
+
+function showItemOnRenderSmoothly(isBox, itemIndex) {
+    var queryItemText = isBox ? "#index-content-item-" + (itemIndex) : "#index-content-item-" + (itemIndex) + "-text";
+    var delayMillisec = isBox ? 0 : 250;
+
+    runEffectToShowItem(queryItemText, delayMillisec);
+}
+
+function showBtnOnRenderSmoothly(itemIndex) {
+    runEffectToShowItem("#index-content-item-" + (itemIndex) + "-btn", 250);
 }
 
 function hideItemOnRender(isBox, itemIndex, isUp) { // isBox: yes = box, no = text / isUp: yes = up, no = down
@@ -32,6 +68,18 @@ function hideItemOnRender(isBox, itemIndex, isUp) { // isBox: yes = box, no = te
     $(queryItemText).css({
         'opacity': 0,
         'transform': transformText,
+        '-webkit-transition': 'opacity 0s ease-in-out, transform 0s',
+        '-moz-transition': 'opacity 0s ease-in-out, transform 0s',
+        '-ms-transition': 'opacity 0s ease-in-out, transform 0s',
+        '-o-transition': 'opacity 0s ease-in-out, transform 0s',
+        'transition': 'opacity 0s ease-in-out, transform 0s'
+    });
+}
+
+function hideBtnOnRender(itemIndex) { // isBox: yes = box, no = text / isUp: yes = up, no = down
+    var queryItemText = "#index-content-item-" + itemIndex + "-btn";
+    $(queryItemText).css({
+        'opacity': 0,
         '-webkit-transition': 'opacity 0s ease-in-out, transform 0s',
         '-moz-transition': 'opacity 0s ease-in-out, transform 0s',
         '-ms-transition': 'opacity 0s ease-in-out, transform 0s',
